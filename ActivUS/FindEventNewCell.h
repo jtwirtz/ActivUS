42a14cf8f446b896ac0cf9a84ebdd4e65c09d5dc
Josie Wirtz <joewirtz@SherryGaosMBP2.lan>
2016-05-01 18:39:10 -0500

    ui changes

---
+++ ActivUS/FindEventNewCell.h
@@ -0,0 +1,13 @@
+//
+//  FindEventNewCell.h
+//  ActivUS
+//
+//  Created by Josie Wirtz on 5/1/16.
+//  Copyright © 2016 Joseph Wirtz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FindEventNewCell : UITableViewCell
+
+@end
---
+++ ActivUS/FindEventNewCell.m
@@ -0,0 +1,24 @@
+//
+//  FindEventNewCell.m
+//  ActivUS
+//
+//  Created by Josie Wirtz on 5/1/16.
+//  Copyright © 2016 Joseph Wirtz. All rights reserved.
+//
+
+#import "FindEventNewCell.h"
+
+@implementation FindEventNewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
